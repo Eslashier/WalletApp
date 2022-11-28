e79c3de6247f4308445710f6949044b89b7e5195
Eslashier <anfelov@gmail.com>
2022-11-28 08:18:29 -0500

    Created first modal

--- src/screens/Loan.tsx
+++ src/screens/Loan.tsx
@@ -1,14 +1,20 @@
 import React, {useState} from 'react';
-import {Text, View} from 'react-native';
+import {Alert, Modal, Pressable, StyleSheet, Text, View} from 'react-native';
 import {LoginButton} from '../components/LoginButton/LogInButton';
 import {styles} from '../theme/RegisterStyle';
 import {InputIcon, InputIconNumber} from '../components/InputIcon/InputIcon';
 import {TextIcon} from '../components/TextIcon/TextIcon';
 
 export const Loan = () => {
+  const [modalVisible, setModalVisible] = useState(true);
   const [loanBalance, setLoanBalance] = useState(50000000);
   const [loan, setLoan] = useState(0);
   const [reason, setReason] = useState('');
+
+  const activateModal = () => {
+    console.log('hola');
+    setModalVisible(true);
+  };
 
   const takeLoan = () => {
     setLoanBalance(loanBalance - loan);
@@ -18,6 +24,16 @@ export const Loan = () => {
   return (
     <>
       <View style={styles.containerForm}>
+        <Modal
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <Text>Hola</Text>
+          </View>
+        </Modal>
         <View>
           <TextIcon
             icon={'dollar-sign'}
@@ -37,7 +53,10 @@ export const Loan = () => {
             setState={setReason}
           />
           <View style={styles.space} />
-          <LoginButton text="Apply for your loan" action={() => takeLoan()} />
+          <LoginButton
+            text="Apply for your loan"
+            action={() => activateModal()}
+          />
         </View>
       </View>
     </>
